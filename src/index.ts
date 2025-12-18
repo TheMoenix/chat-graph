1e19677cae52191bee2651673cfc5b09dc96a8eb
Mohammed Alshami <msalshami420@gmail.com>
2025-12-18 18:50:17 +0300

    export everything

--- src/index.ts
+++ src/index.ts
@@ -7,35 +7,16 @@
  * @packageDocumentation
  */
 
-export { ChatGraph, ChatGraphBuilder } from './graph';
+export * from './graph';
 export type * from './types/graph.types';
-export { START, END } from './constants';
+export * from './constants';
 
 // Schema and state management with Zod
-export type {
-  StateSchema,
-  InferState,
-  ReducerConfig,
-  FieldConfig,
-} from './schema/state-schema';
-export {
-  createInitialState,
-  mergeState,
-  registry,
-  StateRegistry,
-} from './schema/state-schema';
-
-// Re-export Zod for convenience
-export { z } from 'zod';
+export type * from './schema/state-schema';
+export * from './schema/state-schema';
 
 // State manager and persistence
-export {
-  StateManager,
-  getGlobalStateManager,
-  resetGlobalStateManager,
-} from './state-manager';
-export type { StateSnapshot } from './persistence/storage-adapter';
-export { StorageAdapter } from './persistence/storage-adapter';
-export { MemoryStorageAdapter } from './persistence/memory-adapter';
-export { MongoStorageAdapter } from './persistence/mongo-adapter';
-export type { MongoStorageOptions } from './persistence/mongo-adapter';
+export * from './state-manager';
+export * from './persistence/storage-adapter';
+export * from './persistence/memory-adapter';
+export * from './persistence/mongo-adapter';
